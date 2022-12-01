d28c14257d257ad71799037da61fe93745f09626
Ben Hobson <benhobson@Bens-MacBook-Pro.local>
2022-11-30 17:31:28 -0800

    first commit starter code

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/HIRE ME!!!/i);
+  expect(linkElement).toBeInTheDocument();
+});
